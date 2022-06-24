ca3ac50ddf54bf1d7e1d594e14aa489b9ab5a84f
monte-flora <monte.flora@noaa.gov>
2022-06-24 12:37:50 -0500

    uploading deep xai severe notebooks

--- severe/scripts/deep_util.py
+++ severe/scripts/deep_util.py
@@ -35,10 +35,49 @@ def show_sample(ds):
 
     return ds.id.values
 
+def make_plot_saliency(image, mask=None, maskalpha=0.7,
+              cmap=cmocean.cm.balance,
+              title=None):
+
+    fig,axes = plt.subplots(nrows=2,ncols=4, figsize=(10,5), dpi=170)
+    fig.set_facecolor('w')
+
+    titles = ['WV','IR','VIL','VIS']
+    vmins = [-2.5,-2.5,0,0]
+    vmaxs = [2,2,20,4]
+
+    cmaps = ['Blues','turbo','Spectral_r','Greys_r']
+    top_row = axes[0,:]    
+        
+    for i,ax in enumerate(top_row):   
+        ax.imshow(image[:,:,i],
+                  cmap=cmaps[i],vmin=vmins[i],vmax=vmaxs[i], alpha=0.9, aspect='auto')
+        ax.axis('off')
+    last_row = axes[1,:]
+    for i,ax in enumerate(last_row):
+        im = ax.imshow(mask[:,:,i], cmap=cmocean.cm.balance, alpha=0.8)
+        color =  'k'
+        
+        ax.annotate(f'Saliency min = {np.min(mask[:,:,i]): 0.03e}', 
+                    (0.1, 0.1), xycoords='axes fraction', color=color, fontsize=6, 
+                   bbox=dict(facecolor='w', edgecolor='black', boxstyle='round,pad=0.5'))
+        ax.annotate(f'Saliency max = {np.max(mask[:,:,i]): 0.03e}', 
+                    (0.1, 0.2), xycoords='axes fraction', color=color, fontsize=6, 
+                   bbox=dict(facecolor='w', edgecolor='black', boxstyle='round,pad=0.5'))
+        
+        ax.axis('off')
+        ax.set_title(titles[i], fontsize=15)
+    
+    if title is not None:
+        fig.suptitle(title)
+    plt.tight_layout()
+
+    return ax
+
 
 def make_plot(image, mask=None, maskalpha=0.7,
               cmap=cmocean.cm.balance,normalize=False, 
-              mask_thresh=None, title=None):
+              mask_thresh=None, title=None, mode='SHAP'):
 
     fig,axes = plt.subplots(nrows=2,ncols=2, figsize=(8,7), dpi=170)
     fig.set_facecolor('w')
@@ -60,7 +99,7 @@ def make_plot(image, mask=None, maskalpha=0.7,
     
     if mask_thresh is not None:
         mask = np.ma.masked_where(abs(mask)<=mask_thresh, mask)
-        title=fr'Abs(SHAP Values) $\geq$ {mask_thresh}'
+        title=fr'Abs({mode} Values) $\geq$ {mask_thresh}'
 
     for i,ax in enumerate(axes.flat):
         if i == 2:
@@ -70,7 +109,7 @@ def make_plot(image, mask=None, maskalpha=0.7,
             im = image[:,:,i]
             alpha=0.8
             
-        ax.imshow(im,cmap=cmaps[i],vmin=vmins[i],vmax=vmaxs[i], alpha=alpha, aspect='auto')
+        ax.imshow(im, cmap=cmaps[i],vmin=vmins[i],vmax=vmaxs[i], alpha=alpha, aspect='auto')
     
         if normalize:
             im = ax.pcolormesh(mask[:,:,i], cmap='seismic', alpha=1.0, norm=norm)
@@ -81,13 +120,13 @@ def make_plot(image, mask=None, maskalpha=0.7,
             levels = MaxNLocator(nbins=11).tick_values(vmin,vmax)
             norm = BoundaryNorm(levels, ncolors=cmap.N, clip=True)
             im = ax.pcolormesh(mask[:,:,i], cmap='seismic', alpha=1.0, norm=norm)
-            fig.colorbar(im, ax=ax, shrink=0.9, label='SHAP Values')
+            fig.colorbar(im, ax=ax, shrink=0.9, label=f'{mode} Values')
         
         color = 'k' if i < 3 else 'w'
         
-        ax.annotate(f'SHAP min = {np.min(mask[:,:,i]): 0.03f}', 
+        ax.annotate(f'{mode} min = {np.min(mask[:,:,i]): 0.03f}', 
                     (0.1, 0.9), xycoords='axes fraction', color=color, fontsize=8)
-        ax.annotate(f'SHAP max = {np.max(mask[:,:,i]): 0.03f}', 
+        ax.annotate(f'{mode} max = {np.max(mask[:,:,i]): 0.03f}', 
                     (0.1, 0.85), xycoords='axes fraction', color=color, fontsize=8)
         
         ax.axis('off')
@@ -95,7 +134,7 @@ def make_plot(image, mask=None, maskalpha=0.7,
 
     # Add colorbar
     if normalize:
-        fig.colorbar(im, ax=axes.ravel().tolist(), shrink=0.9, label='SHAP Values')    
+        fig.colorbar(im, ax=axes.ravel().tolist(), shrink=0.9, label=f'{mode} Values')    
     
     
     if title is not None:
@@ -103,35 +142,6 @@ def make_plot(image, mask=None, maskalpha=0.7,
     #plt.tight_layout()
 
     return ax
-
-def cdf_plot(im, shap_values):
-
-    fig = plt.figure()
-    fig.set_facecolor('w')
-    colors = np.array(['b','r','y','k'])
-    labels = np.array(['WV','IR','VIL','VIS'])
-    for i in np.arange(0,4):
-        s_tmp = copy.deepcopy(shap_values[:,:,i].ravel())
-        idx_to_sort = np.argsort(im[:,:,i].ravel())
-        s_tmp = s_tmp.cumsum()
-        plt.plot(class_explainer.expected_value + s_tmp,color=colors[i],label=labels[i])
-        plt.plot(len(s_tmp),class_explainer.expected_value + s_tmp[-1],'o',
-           color=colors[i],markerfacecolor='w')
-        anno = str(np.round(s_tmp[-1],2))
-        plt.text(len(s_tmp)+50,class_explainer.expected_value + s_tmp[-1]-0.005,
-           anno)
-  
-        anno = '$\mathrm{\mathbb{E}}[x]$:' + str(np.round(class_explainer.expected_value[0],2))
-
-    plt.text(100, class_explainer.expected_value[0]+.07,
-           anno)
-  
-    plt.legend(loc=2)
-    plt.ylabel('Output Probability')
-    plt.xlabel('Pixel ID')
-    plt.xlim([0,len(s_tmp)+300])
-    plt.title('Shap Image CDF')
-
 
 def minmax(X):
   X_new = copy.deepcopy(X)
